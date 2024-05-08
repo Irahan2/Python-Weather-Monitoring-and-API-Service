440f12cbdea495f4e3577c324158171f2b256b65
Irahan2 <118784594+Irahan2@users.noreply.github.com>
2024-05-08 12:44:14 +0200

    Updated

---
+++ Main.py
@@ -0,0 +1,66 @@
+import requests
+import time
+import json
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pytz
+
+
+def collect_data():
+    url = 'http://innov8dev.com/sampleapi/getdata.php'
+    log_file = 'sensor_data.log'
+
+    for _ in range(300):
+        response = requests.get(url)
+        data = response.json()
+
+        timestamp = data['timestamp']
+        mst_time = datetime.fromtimestamp(timestamp, pytz.timezone('MST'))
+        data['timestamp'] = mst_time.strftime('%Y-%m-%d %H:%M:%S')
+
+        with open(log_file, 'a') as file:
+            file.write(json.dumps(data) + '\n')
+
+        time.sleep(2)
+
+
+def plot_data():
+    # Read data from log file
+    with open('sensor_data.log', 'r') as file:
+        lines = file.readlines()
+
+    timestamps = []
+    temperatures = []
+    rains = []
+
+    # Process each line of data
+    for line in lines:
+        data = json.loads(line)
+        timestamps.append(data['timestamp'])
+        temperatures.append(data['temperature'])
+        rains.append(data['rain'])
+
+    # Plotting
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Temperature', color='tab:red')
+    ax1.plot(timestamps, temperatures, 'r-')
+    ax1.tick_params(axis='y', labelcolor='tab:red')
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Rain', color='tab:blue')
+    ax2.plot(timestamps, rains, 'b-')
+    ax2.tick_params(axis='y', labelcolor='tab:blue')
+
+    # Formatting and saving the plot
+    fig.tight_layout()
+    plt.xticks(rotation=45)
+    plt.title('Temperature and Rain Data Over Time')
+    plt.savefig('temperature_and_rain_chart.jpg')
+    plt.show()
+
+
+if __name__ == "__main__":
+    collect_data()
+    plot_data()
